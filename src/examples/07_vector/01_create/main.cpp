3f6d5c90a69bbc86937c51fa439ea7bcbed66a57
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-06-14 00:54:56 +0000

    vector memory size and capacity

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,29 @@
+#include<vector>
+#include<iostream>
+
+using std::cout;
+using std::vector;
+
 int main()
 {
+    vector<int> nums{1, 10, 7};
+
+    cout<<nums[0]<<"\n";
+    nums[0] =  5;
+    cout<<nums[0]<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+
+    nums.push_back(20);
+    cout<<"Size: "<<nums.size()<<"\n";
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+
+    nums.push_back(15);
+    nums.push_back(11);
+    nums.push_back(21);
+
+    cout<<"Size: "<<nums.size()<<"\n";
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+
     return 0;
 }
