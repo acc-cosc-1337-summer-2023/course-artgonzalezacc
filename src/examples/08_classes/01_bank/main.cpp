3a0bd32231a4382ac22c2d4b89a86d213e695e31
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-06-16 13:27:12 +0000

    class bank account and test cases

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,23 @@
+#include "bank_account.h"
+#include<iostream>
+
+using std::cin;
+using std::cout;
 
 int main()
 {
+	BankAccount account;
+	auto amount = 0;
+
+	cout<<"\nBalance: "<<account.get_balance()<<"\n";
+
+	cout<<"Enter an amount: ";
+	cin>>amount;
+
+	cout<<"Deposit: ";
+	account.deposit(amount);
+
+	cout<<"\nBalance: "<<account.get_balance()<<"\n";
 
 	return 0;
 }
