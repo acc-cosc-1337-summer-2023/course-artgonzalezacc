f2793e41aeee4fe79447a31698cf448c2125b562
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-06-21 00:49:16 +0000

    use vector for atm run menu

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,15 +1,26 @@
 #include "atm.h"
 #include "bank_account.h"
+#include "checking_account.h"
+#include "savings_account.h"
 #include<iostream>
+#include<vector>
 
 using std::cin;
 using std::cout;
+using std::vector;
 
 int main()
 {
-	BankAccount account(100);
+	std::vector<BankAccount> accounts;
+	
+	accounts.push_back(CheckingAccount(100));
 
-	run_menu(account);
+	accounts.push_back(SavingsAccount(300));
+
+	run_menu(accounts);
+
+	cout<<accounts[0].get_balance()<<"\n";
+	cout<<accounts[0].get_balance()<<"\n";
 
 	return 0;
 }
--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "bank_account.h"
+#include "checking_account.h"
+#include "savings_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -8,30 +10,30 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test account get balance initialization")
 {
-	BankAccount account;
+	SavingsAccount account;
 
-	REQUIRE(account.get_balance() == 0);
+	REQUIRE(account.get_balance() == 5);
 }
 
 TEST_CASE("Test account get balance with param constructor")
 {
-	BankAccount account(100);
+	CheckingAccount account(100);
 
 	REQUIRE(account.get_balance() == 100);
 }
 
 TEST_CASE("Test account deposit")
 {
-	BankAccount account(100);
-	REQUIRE(account.get_balance() == 100);
+	SavingsAccount account(100);
+	REQUIRE(account.get_balance() == 105);
 
 	account.deposit(50);
-	REQUIRE(account.get_balance() == 150);
+	REQUIRE(account.get_balance() == 155);
 }
 
 TEST_CASE("Test account deposit value less than 0")
 {
-	BankAccount account(100);
+	CheckingAccount account(100);
 	REQUIRE(account.get_balance() == 100);
 
 	account.deposit(-50);
@@ -40,16 +42,16 @@ TEST_CASE("Test account deposit value less than 0")
 
 TEST_CASE("Test account withdraw")
 {
-	BankAccount account(100);
-	REQUIRE(account.get_balance() == 100);
+	SavingsAccount account(100);
+	REQUIRE(account.get_balance() == 105);
 	
 	account.withdraw(25);
-	REQUIRE(account.get_balance() == 75);
+	REQUIRE(account.get_balance() == 80);
 }
 
 TEST_CASE("Test account withdraw with amount less than 0")
 {
-	BankAccount account(100);
+	CheckingAccount account(100);
 	REQUIRE(account.get_balance() == 100);
 
 	account.withdraw(-25);
