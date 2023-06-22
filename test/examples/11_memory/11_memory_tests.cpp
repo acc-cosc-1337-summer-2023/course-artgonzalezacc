3ae276a8f83237d6517742a69018c9082d92a7b6
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-06-22 23:24:19 +0000

    reference and pointer params

--- test/examples/11_memory/11_memory_tests.cpp
+++ test/examples/11_memory/11_memory_tests.cpp
@@ -1,6 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "ref_pointers.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test reference and pointer parameters")
+{
+	int num1 = 1, num2 = 2;
+	ref_ptr_params(num1, &num2);
+
+	REQUIRE(num1 == 5);
+	REQUIRE(num2 == 10);
 }
