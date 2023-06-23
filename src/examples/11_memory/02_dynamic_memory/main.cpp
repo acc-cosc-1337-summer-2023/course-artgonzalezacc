bee6902a218368b9b410796bbb3c8712a323668a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-06-23 00:17:57 +0000

    dynamic memory w pointer example

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,5 +1,15 @@
+#include<iostream>
+
+using std::cout;
+
 int main() 
 {
+	//1--create memory
+	int* int_ptr = new int(10);//creates memory on the heap dynamically
+	//2-use memory
+	cout<<*int_ptr<<"\n";
+	//3-free or delete memory from the heap/free store
+	delete int_ptr;
 	
 	return 0;
 }
