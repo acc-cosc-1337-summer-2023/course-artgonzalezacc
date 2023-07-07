e8304cf868dd9c86487fc382e91e61c4f17b015d
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-07-07 00:27:30 +0000

    stack arrays, pointers, and parameters

--- src/examples/10_arrays/01_arrays_mem/main.cpp
+++ src/examples/10_arrays/01_arrays_mem/main.cpp
@@ -1,9 +1,16 @@
-
+#include "arrays_mem.h"
 
 int main() 
 {
 
-	
+	//use_stack_array();
+	//array_months();
+	//arrays_and_pointers();
+	const auto SIZE = 4;
+    int numbers[SIZE]{1,2,3, -1};
+
+	//display_array(numbers);
+	display_array(numbers, SIZE);
 
 	return 0;
 
