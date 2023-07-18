d89692a89a2263184e96f0084fbbd88c07d32ac6
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-07-18 23:26:45 +0000

    lvalue and rvalue

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -4,9 +4,12 @@ using std::cout;
 
 int main()
 {
-    cout<<"Create Vector v on the stack\n";
+    /*cout<<"Create Vector v on the stack\n";
     Vector v(3);//on the heap
-    cout<<"Vector v removed from the stack\n";
+    cout<<"Vector v removed from the stack\n";*/
+    int num = 5;//num is LVALUE
+    int& num_ref = num;
+    //5 = num; 5 is rvalue
     
     return 0;
 }
