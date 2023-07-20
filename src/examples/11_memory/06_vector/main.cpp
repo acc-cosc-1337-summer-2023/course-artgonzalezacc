78e5b721c21bf645b3f661c865c19bab5017c40c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-07-20 23:19:50 +0000

    template function

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -1,15 +1,28 @@
 #include "my_vector.h"
+#include<string>
 //
-using std::cout;
+using std::cout; using std::string;
+
+template<typename T>
+T add(T value1, T value2)
+{
+    return value1 + value2;
+}
 
 int main()
-{
-    Vector v(3);
-    v.PushBack(5);
-    cout<<v[0]<<"\n";
+{   
+    auto result = add(5, 10);
+    cout<<"Result: "<<result<<"\n";
 
-    v.PushBack(10);
-    cout<<v[1]<<"\n";
+    auto result1 = add(10.5, 5.3);
+    cout<<"Result: "<<result1<<"\n";
+
+    string v1 = "C++ ";
+    string v2 = "rocks";
+
+    auto result2 = add(v1, v2);
+    cout<<"Result: "<<result2<<"\n";
+
 
     return 0;
 }
